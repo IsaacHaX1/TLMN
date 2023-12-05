c1e6be6fc35f5880bb2988d07e78bf68eb565af6
IsaacHaX1 <150787188+IsaacHaX1@users.noreply.github.com>
2023-12-06 01:06:03 +0700

    socket io

--- SERVER/public/xuly.js
+++ SERVER/public/xuly.js
@@ -1,7 +1,38 @@
 var socket = io("http://localhost:3000");
+socket.on("server-send-dki-thatbai",function(){
+    alert("sai username co nguoi dang ky roi ... ")
+});
+socket.on("server-send-dki-thanhcong",function(data){
+    $("#currentUser").html(data);
+    $("#loginForm").hide(2000);
+    $("#chatForm").show(1000);
+    
+
+});
+socket.on("server-send-danhsach_Users",function(data){
+    console.log(data);
+    $("#boxContent").html("");
+    data.forEach(function(i){
+        $("#boxContent").append("<div class='user'>"+i+"</div>");
+    });
+})
 $(document).ready(function(){
    // alert(1);
    $("#loginForm").show();
    $("#chatForm").hide();
+   $("#btnRegister").click(function(){
+    alert($("#txtUsername").val());
+    socket.emit("client-send-Username",$("#txtUsername").val());
+   })
+
+   $("#btnLogout").click(function(){
+    socket.emit("logout");
+    $("#chatForm").hide(2000);
+    $("#loginForm").show(1000);
+   })
+
+   $("#btnSendMessage").click(function(){
+    socket.emit("user-send-message", $("txtMessage").val());
+   })
 
 })
--- SERVER/server.js
+++ SERVER/server.js
@@ -7,9 +7,33 @@ app.set("views","./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
+var mangUser = [];
 
 io.on("connection",function(socket){
-    console.log("Co nguoi ket noi "+socket.id)
+    console.log("Co nguoi ket noi "+socket.id);
+    socket.on("client-send-Username",function(data){
+        console.log(data);
+        if(mangUser.indexOf(data)>=0){ // xem xo dât trong mang khong
+            socket.emit("server-send-dki-thatbai");
+        }else{
+            mangUser.push(data);
+            socket.Username = data;
+            socket.emit("server-send-dki-thanhcong", data);
+            io.sockets.emit("server-send-danhsach_Users", mangUser);
+
+        }
+    });
+
+    socket.on("logout",function(){
+        mangUser.splice(
+            mangUser.indexOf(socket.Username),1
+        );
+        socket.broadcast.emit("server-send-danhsach_Users", mangUser);
+    })
+
+    socket.on("user-send-message",function(data){
+        
+    })
 })
 app.get("/",function(reg,res){
     res.render("trangchu");
