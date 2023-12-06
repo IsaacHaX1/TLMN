e2e0457842e1fc3cf9dd2b5b8c01d8685767af89
IsaacHaX1 <150787188+IsaacHaX1@users.noreply.github.com>
2023-12-07 00:02:41 +0700

    room khoi tao

---
+++ ROOM/public/xuly.js
@@ -0,0 +1,15 @@
+var socket = io("http://localhost:3000");
+socket.on("server-send-dki-thatbai",function(){
+    
+});
+socket.on("server-send-dki-thanhcong",function(data){
+
+    
+
+});
+
+$(document).ready(function(){
+   // alert(1);
+
+
+})
---
+++ ROOM/server.js
@@ -0,0 +1,19 @@
+const { log } = require('console');
+const express = require('express')
+const app = express()
+app.use(express.static("./public"));
+app.set("view engine","ejs");
+app.set("views","./views");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+var mangUser = [];
+
+io.on("connection",function(socket){
+    console.log("Co nguoi ket noi "+socket.id);
+    console.log(socket.adapter.room);
+
+})
+app.get("/",function(reg,res){
+    res.render("trangchu");
+});
