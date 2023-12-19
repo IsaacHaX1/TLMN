eaf6cabe0af303bbb90114d0dbfb986fa52fd844
IsaacHa <mbiz.issac@gmail.com>
2023-12-19 15:36:51 +0700

    coommit

--- GSERVER/server.js
+++ GSERVER/server.js
@@ -27,7 +27,17 @@ var romfunction = {
             return [false];
 
 } else return [false];
-}
+    },
+    getRoom(i, _myid) {
+       // romfunction.getRoom(i, _myid)
+        let obj = new Object();
+        obj.id = roomsGame[i].id;
+        obj.phong = roomsGame[i].phong;
+        obj.idsend = roomsGame[i].idsend;
+        obj.myid = _myid;
+        return obj;
+
+    }
 }
 
 function myFunction(p1, p2) {
@@ -38,15 +48,20 @@ io.on('connection', function (socket) {
 
     socket.on("tao-room", function (data) {
         let checkroom = romfunction.checkRooms();
-        console.log(checkroom);
         if (checkroom[0]) {
             let idz = checkroom[2];
             socket.join(checkroom[1].phong);
             socket.phong = checkroom[1].phong;
-            roomsGame[checkroom[2]].id.push(checkroom[1].id[0]);
-            console.log(roomsGame);
-
-            socket.emit("tao-room-return", roomsGame[idz]);
+            roomsGame[idz].id.push(socket.id);
+            let returnRoom = roomsGame[idz];
+            returnRoom.idsend = socket.id;
+            returnRoom.myid = socket.id;
+            let idc = romfunction.getRoom(idz, returnRoom.id[0])
+            socket.emit("tao-room-return", returnRoom);
+            //io.sockets.in(socket.phong).emit("tao-room-return", returnRoom);
+            // gui cho id cu the
+            io.to(returnRoom.id[0]).emit("tao-room-return", idc);
+            // gui cho tat ca id con lai socket.broadcast.emit
 
         } else {
             let ran = Math.floor(Math.random() * 10) + "" + (Math.random() + 1).toString(36).substring(7);
@@ -58,6 +73,17 @@ io.on('connection', function (socket) {
             obj.phong = socket.phong;
             obj.id = [];
             obj.id.push(socket.id);
+            //obj.idchuphong = socket.id;
+            obj.idsend = socket.id;
+            obj.myid = socket.id;
+            obj.gamePlay = [];
+            for (let i = 0; i < 10; i++) {
+                obj.gamePlay[i] = [];
+                for (let j = 0; j < 20; j++) {
+                    obj.gamePlay[i][j] = 0;
+                }
+
+            }
             roomsGame.push(obj);
             socket.emit("tao-room-return", roomsGame[roomsGame.length -1]);
         }
@@ -68,7 +94,7 @@ io.on('connection', function (socket) {
     })
 
     socket.on("danh-co", function (data) {
-        io.sockets.in(socket.phong).emit("danh-co-return",data)
+        io.sockets.in(socket.phong).emit("danh-co-return", socket.id)
     })
 
     socket.on("Client-send-data", function (data) {
